db95f0f1a218552c79f99fa43bb43101c120798d
patryk dziedzioch <patrykdziedzioch@gmail.com>
2020-05-19 20:17:43 +0200

    Added function for Health and Mana bars

--- src/Player.gd
+++ src/Player.gd
@@ -1,5 +1,6 @@
 extends KinematicBody2D
 signal hit
+signal player_stats_changed
 
 export var speed = 400  # How fast the player will move (pixels/sec).
 var screen_size  # Size of the game window.
@@ -12,11 +13,12 @@ var health_regeneration = 1
 var mana = 100
 var mana_max = 100
 var mana_regeneration = 2
-
+var attack_playing = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	emit_signal("player_stats_changed", self)
 
 
 func _process(delta):
@@ -47,18 +49,38 @@ func _process(delta):
 		$AnimatedSprite.flip_v = false
 		# See the note below about boolean assignment
 		$AnimatedSprite.flip_h = velocity.x < 0
-		
-		
+	
+	# Regenerates player mana
+	var new_mana = min(mana + mana_regeneration * delta, mana_max)
+	if new_mana != mana:
+		mana = new_mana
+		emit_signal("player_stats_changed", self)
+
+	# Regenerates player health
+	var new_health = min(health + health_regeneration * delta, health_max)
+	if new_health != health:
+		health = new_health
+		emit_signal("player_stats_changed", self)
+
+
 func _on_AnimatedSprite_animation_finished():
 	if $AnimatedSprite.animation != 'run':
 		$AnimatedSprite.play('idle')
 
+
 func _input(event):
-	if event is InputEventKey:
-		if event.scancode == KEY_A:
-			set_process_input(false)
-			npc.talk("A")
-		elif event.scancode == KEY_B:
-			set_process_input(false)
-			npc.talk("B")
-				
+	if event.is_action_pressed("attack"):
+		attack_playing = true
+					#var animation = get_animation_direction(last_direction) + "_attack"
+					#$Sprite.play(animation)
+		#TODO Add attack animation
+		$AnimatedSprite.play('idle')
+	elif event.is_action_pressed("ui_right"): # needs a shoot animation and key
+		if mana >= 25:
+			mana = mana - 25
+			emit_signal("player_stats_changed", self)
+			attack_playing = true
+			#TODO Add attack animation
+			#var animation = get_animation_direction(last_direction) + "_fireball"
+			#$Sprite.play(animation)
+
